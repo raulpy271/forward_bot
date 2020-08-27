51699ea2913eefcbd84e7a0db909a0f165f7cc21
Raul <raulrufino0@gmail.com>
2020-08-27 11:02:32 -0300

    adding functions and refactoring

--- handlers.py
+++ handlers.py
@@ -7,35 +7,68 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+def is_channel_or_group(chat):
+    return chat.title != None
+
+
 def start(update, context):
     logger.info("started")
     update.message.reply_text('Hi!')
 
 
 def unauthorized_access(update, context):
-    text = "Você não tem acesso a esse bot!"
+    text = "you don't have access to this bot!"
     update.message.reply_text(text)
 
 
 def reply_user_id(update, context):
     user = update.effective_user
-    text = "seu id é: " + str(user.id)
+    text = "your id is: " + str(user.id)
     update.message.reply_text(text)
 
 
-def add_chat_id_to_forward_list(bot, chat_id):
-    bot.forward_list.append(chat_id)
+def add_chat_to_forward_list (bot, chat):
+    bot.forward_list.append(chat)
 
 
-def add_this_group_to_forward_list(update, context):
+def remove_chat_to_forward_list (bot, chat):
+    bot.forward_list.remove(chat)
+
+
+def add_this_group_to_forward_list_and_reply_the_result(update, context):
     chat = update.effective_chat
-    add_chat_id_to_forward_list(context.bot, chat.id)
-    logger.info(str(chat.id) + " has added to forward list")
+    bot = context.bot
+    if is_channel_or_group(chat):
+        if not chat in bot.forward_list:
+            add_chat_to_forward_list(context.bot, chat)
+            text = "this chat was successfully added"
+        else:
+            text = "this chat was already on the list"
+    else:
+        text = "This chat no has a group o channel"
+    update.message.reply_text(text)
+
+
+def remove_this_group_to_forward_list_and_reply_the_result (update, context):
+    chat = update.effective_chat
+    bot = context.bot
+    if is_channel_or_group(chat):
+        if chat in bot.forward_list:
+            remove_chat_to_forward_list(context.bot, chat)
+            text = "this chat was successfully removed"
+        else:
+            text = "this chat is not on the list"
+    else:
+        text = "This chat no has a group o channel"
+    update.message.reply_text(text)
+
+
 
 
 def view_forward_list (update, context):
     forward_list = context.bot.forward_list
-    text = "this are the groups in forward list:\n" + str(forward_list)
+    forward_list_formated = prettify_forward_list(forward_list)
+    text = "this are the groups in forward list:" + forward_list_formated
     update.message.reply_text(text)
 
 
@@ -50,6 +83,13 @@ def forward_message_for_chat (update, context, chat_id):
 
 def forward_message_for_all_chats_in_forward_list( update, context):
     list_of_chats = context.bot.forward_list
-    for chat_id in list_of_chats:
-        forward_message_for_chat (update, context, chat_id)
+    for chat in list_of_chats:
+        forward_message_for_chat (update, context, chat.id)
+
+
+def prettify_forward_list (forward_list):
+    result = "\n"
+    for chat in forward_list:
+        result += " - " + chat.title + "\n"
+    return result
 
--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from handlers import (
     view_forward_list,
     reply_user_id,
     forward_message_for_chat,
-    add_this_group_to_forward_list,
+    remove_this_group_to_forward_list_and_reply_the_result,
+    add_this_group_to_forward_list_and_reply_the_result,
     forward_message_for_all_chats_in_forward_list)
 
 
@@ -19,12 +20,11 @@ user_info = User_Info()
 token = user_info.token
 
 
-
 def main():
     updater = Updater(token, use_context=True)
     dp = updater.dispatcher
     bot = dp.bot
-    bot.forward_list = [-320128406, -463344466]
+    bot.forward_list = []
     forward_handler = MessageHandler(
         Filters.all,
         forward_message_for_all_chats_in_forward_list
@@ -33,8 +33,15 @@ def main():
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("id", reply_user_id))
-    dp.add_handler(CommandHandler("add", add_this_group_to_forward_list))
     dp.add_handler(CommandHandler("view_forward_list", view_forward_list))
+    dp.add_handler(CommandHandler(
+        "add", 
+        add_this_group_to_forward_list_and_reply_the_result)
+    )
+    dp.add_handler(CommandHandler(
+        "remove", 
+        remove_this_group_to_forward_list_and_reply_the_result)
+    )
 
     dp.add_handler(forward_handler)
 
