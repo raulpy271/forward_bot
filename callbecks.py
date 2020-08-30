0fbd446888be91a379114d21f8cca69e76af4620
Raul <raulrufino0@gmail.com>
2020-08-30 15:25:31 -0300

    using decorators to make the code more clean

--- callbecks.py
+++ callbecks.py
@@ -4,108 +4,96 @@ from tools.callbecks_helper import (
     forward_message_for_chat, 
     search_chat_with_this_title, 
     get_forward_list,
-    prettify_forward_list)
+    get_waiting_list,
+    prettify_forward_list,
+    user_have_forward_list,
+    execute_if_bot_are_new_member,
+    execute_if_user_have_forward_list)
 
 
-start_text = "Hello"
-
+start_text = (
+"""Hello World, I'm forward_bot!
+I will help you forward your messages to many groups and channels at once.
+Type \help to see how make it.
+""")
 
 def start(update, context):
     user = update.effective_user
     create_users_variables_in_forwarding_list_of_all_users(user)
-    update.message.reply_text(start_text)
+    update.message.reply_text(start_text, parse_mode="Markdown")
 
 
 def add_chat_to_waiting_list (user, title):
-    user_id_in_str = str(user.id)
-    (forwarding_list_of_all_users[user_id_in_str][
-        "title_of_chats_waiting_to_be_added"]).append(title)
+    get_waiting_list(user).append(title)
+
     
-
-
 def add_chat_to_forward_list (user, chat):
-    user_id_in_str = str(user.id)
-    (forwarding_list_of_all_users[user_id_in_str][
-        "forward_list"]).append(chat)
+    get_forward_list(user).append(chat)
 
 
 def remove_chat_to_forward_list (user, chat):
-    user_id_in_str = str(user.id)
-    (forwarding_list_of_all_users[user_id_in_str][
-        "forward_list"]).remove(chat)
+    get_forward_list(user).remove(chat)
     chat.leave()
 
 
 def remove_chat_to_waiting_list (user, title):
-    user_id_in_str = str(user.id)
-    (forwarding_list_of_all_users[user_id_in_str][
-        "title_of_chats_waiting_to_be_added"]).remove(title)
-
+    get_waiting_list(user).remove(title)
 
 
 def add_group_title_to_waiting_list(update, context):
     user = update.effective_user
-    user_id_in_str = str(user.id)
-    if not user_id_in_str in forwarding_list_of_all_users:
-        create_users_variables_in_forwarding_list_of_all_users(user)
+    create_users_variables_in_forwarding_list_of_all_users(user)
     title = " ".join(context.args)
     add_chat_to_waiting_list(user, title)
     text = "Link stored, for end this process, add this bot in your chat"
     update.message.reply_text(text)
 
 
+@execute_if_user_have_forward_list
 def remove_group_to_forward_list (update, context):
     user = update.effective_user
     title = " ".join(context.args)
-    user_id_in_str = str(user.id)
-    if user_id_in_str in forwarding_list_of_all_users:
-        forward_list = forwarding_list_of_all_users[
-            user_id_in_str][ "forward_list"]
-        chat = search_chat_with_this_title(forward_list, title)
-        if chat == None:
-            text = "Invalid Link or this link not in the list"
-        else:
-            remove_chat_to_forward_list(user, chat)
-            text = "Chat removed, i'm left this chat now"
+    forward_list = get_forward_list(user) 
+    chat = search_chat_with_this_title(forward_list, title)
+    if chat == None:
+        text = (
+
+""" This chat title is not on your forward list.
+Type \show to see what on it""") 
+
     else:
-        text = """You don't have a forwarding list yet. 
-            Call the /start command to see how use this bot"""
+        remove_chat_to_forward_list(user, chat)
+        text = "Chat removed, i'm left this chat now"
     update.message.reply_text(text)
 
 
+@execute_if_user_have_forward_list
 def view_forward_list (update, context):
     user = update.effective_user
-    user_id_in_str = str(user.id)
-    if user_id_in_str in forwarding_list_of_all_users:
-        forward_list = get_forward_list(user)
-        forward_list_formated = prettify_forward_list(forward_list)
-        text = "this are the groups in forward list:" + forward_list_formated
-    else:
-        text = """You don't have a forwarding list yet. 
-            Call the /start command to see how use this bot"""
-    update.message.reply_text(text)
+    forward_list = get_forward_list(user)
+    forward_list_formated = prettify_forward_list(forward_list)
+    text = "this are the groups in forward list:" + forward_list_formated
+    update.message.reply_text(text, parse_mode="Markdown")
 
 
+@execute_if_user_have_forward_list
 def forward_message_for_all_chats_in_forward_list( update, context):
     user = update.effective_user
-    user_id_in_str = str(user.id)
-    if user_id_in_str in forwarding_list_of_all_users:
-        text = "Forwarding message"
-        list_of_chats = get_forward_list(user)
-        for chat in list_of_chats:
-            forward_message_for_chat (update, context, chat.id)
-    else:
-        text = """You don't have a forwarding list yet. 
-            Call the /start command to see how use this bot"""
-    update.message.reply_text(text)
+    forward_list = get_forward_list(user)
+    forward_list_formated = prettify_forward_list(forward_list)
+    text = "Forwarding message for:" + forward_list_formated
+    for chat in forward_list:
+        forward_message_for_chat (update, context, chat.id)
+    update.message.reply_text(text, parse_mode="Markdown")
 
 
+@execute_if_bot_are_new_member
 def add_group_to_forward_list(update, context):
     user = update.effective_user
     chat = update.effective_chat
     add_chat_to_forward_list(user, chat)
     remove_chat_to_waiting_list(user, chat.title)
-    text = "chat added to forward list"
-    user.send_message(text)
+    text = "*" + chat.title + "*" + " added to forward list"
+    user.send_message(text, parse_mode="Markdown")
 
 
--- tools/callbecks_helper.py
+++ tools/callbecks_helper.py
@@ -8,6 +8,14 @@ def get_forward_list(user):
     return forward_list
 
 
+def get_waiting_list(user):
+    user_id_in_str = str(user.id)
+    waiting_list = forwarding_list_of_all_users[
+        user_id_in_str]["title_of_chats_waiting_to_be_added"]
+    return waiting_list 
+
+
+
 def forward_message_for_chat (update, context, chat_id):
     user = update.effective_user
     message = update.message
@@ -33,7 +41,47 @@ def create_users_variables_in_forwarding_list_of_all_users(user):
 def prettify_forward_list (forward_list):
     result = "\n"
     for chat in forward_list:
-        result += chat.title + "\n"
+        result += "*" + chat.title + "*\n"
     return result
+
+
+def user_have_forward_list(user):
+    user_id_in_str = str(user.id)
+    return user_id_in_str in forwarding_list_of_all_users
+
+
+def forward_list_is_empy(user):
+    if user_have_forward_list(user):
+        return not bool(get_forward_list(user))
+    else: return True
+
+
+def execute_if_user_have_forward_list(callback):
+    def execute_if_user_have_forward_list_and_reply_to_user (update, context):
+        user = update.effective_user
+        if not forward_list_is_empy(user):
+            callback(update, context)
+        else:
+            text = (
+
+"""You don't have a forwarding list yet.
+Call the /start command to see how use this bot""")
+
+            update.message.reply_text(text) 
+
+
+    return execute_if_user_have_forward_list_and_reply_to_user
+
+
+def execute_if_bot_are_new_member(calback):
+    def execute_if_bot_are_in_new_members_list (update, context):
+        bot = context.bot
+        message = update.message
+        if bot in message.new_chat_members:
+            callback(update, context)
+        else: return
+
+
+    return execute_if_bot_are_in_new_members_list 
 
 
